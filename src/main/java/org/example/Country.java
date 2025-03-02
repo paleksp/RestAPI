10f2e9267039b69287a5e9460bbb704e1d9089e6
Alexey <pivovarov.vrn@mail.com>
2025-03-02 16:50:37 +0300

    init

---
+++ src/main/java/org/example/Country.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.springframework.web.client.RestClient;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public class Country {
+    public static void main(String[] args) {
+        System.out.printf("Ввведите страну:");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String url = "https://restcountries.com/v3.1/alpha/"+input;
+
+        RestClient restClient = RestClient.create();
+        String response = restClient.
+                get().
+                uri(url).
+                retrieve().
+                body(String.class);
+
+        System.out.println("Ответ:" + response);
+    }
+}
